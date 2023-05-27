f75f29bb5838aa9be532b889c2ec4883b54881ac
NAKGIOE.ORG <nakigoetenshi@gmail.com>
2023-05-27 21:16:40 +0500

    nuget prerendering test

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
+
+// extract the service-registration process to the static local function
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+  services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+}
